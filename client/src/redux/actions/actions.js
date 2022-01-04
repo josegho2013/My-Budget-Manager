b6847e078da940e68c4010fe743118296cee2cac
jose hernandez <josegho2013@gmail.com>
2022-01-04 13:22:47 -0300

    actions and reducer

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+export const ADD_USER = "ADD_USER";
+export const GET_USER = "GET_USER";
+export const UPDATE_USER = "UPDATE_USER";
+export const DELETE_USER = "DELETE_USER";
+export const ADD_OPEARATIONS = "ADD_OPEARATIONS";
+export const GET_OPERATIONS = "GET_OPERATIONS";
+export const UPDATE_OPERATIONS = "UPDATE_OPERATIONS";
+export const DELETE_OPERATIONS = "DELETE_OPERATIONS";
+
+export function addUser() {
+  return async (dispatch) => {
+    const { data } = await axios.post(`/user/create`);
+    dispatch({ type: "ADD_USER", payload: data });
+  };
+}
+
+export function getUser() {
+  return async (dispatch) => {
+    const { data } = await axios.get(`/user`);
+    dispatch({ type: "GET_USER", payload: data });
+  };
+}
+
+export function updateUser() {
+  return async (dispatch) => {
+    const { data } = await axios.put(`/user/update/:id`);
+    dispatch({ type: "UPDATE_USER", payload: data });
+  };
+}
+
+export function delteUser() {
+  return async (dispatch) => {
+    const { data } = await axios.delete(`/user/delete/:id`);
+    dispatch({ type: "DELETE_USER", payload: data });
+  };
+}
+
+
+export function addOperations() {
+  return async (dispatch) => {
+    const { data } = await axios.post(`/operations/create`);
+    dispatch({ type: "ADD_OPERATIONS", payload: data });
+  };
+}
+
+export function getOperationsDb() {
+  return async (dispatch) => {
+    const { data } = await axios.get(`/operations`);
+    dispatch({ type: "GET_OPERATIONS", payload: data });
+  };
+}
+
+export function updateOperations() {
+  return async (dispatch) => {
+    const { data } = await axios.put(`/operations/update/:id`);
+    dispatch({ type: "UPDATE_OPERATIONS", payload: data });
+  };
+}
+
+export function delteOperations() {
+  return async (dispatch) => {
+    const { data } = await axios.delete(`/operations/delete/:id`);
+    dispatch({ type: "DELETE_OPERATIONS", payload: data });
+  };
+}
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,182 +1,78 @@
 import {
-    GET_All_DOGS,
-  
-  } from "../actions/actions";
-  
-  const initialState = {
-    dogsAll: [],
-    addDogs: [],
-    temperament: [],
-    searchById: [],
-    requestDogs: [],
-    dogDelete: false,
-  };
-  function RootReducer(state = initialState, action) {
-    switch (action.type) {
-      case GET_All_DOGS:
+  ADD_USER,
+  GET_USER,
+  UPDATE_USER,
+  DELETE_USER,
+  ADD_OPEARATIONS,
+  GET_OPERATIONS,
+  UPDATE_OPERATIONS,
+  DELETE_OPERATIONS,
+} from "../actions/actions";
+
+const initialState = {
+  getUser: [],
+  addUser: [],
+  upateUser: [],
+  deleteUser:false,
+  getOperations: [],
+  addOperations: [],
+  upateOperations: [],
+  deleteOperations:false,
+ 
+};
+function RootReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        ...state,
+        addUser: action.payload,
+      };
+    case GET_USER:
+      return {
+        ...state,
+        getUser: action.payload,
+      };
+    case UPDATE_USER:
+      return {
+        ...state,
+        upateUser: action.payload,
+      };
+    case DELETE_USER:
+      return {
+        ...state,
+        deleteUser: action.payload,
+      };
+
+/////////////////////////////////////////////
+
+
+      case ADD_OPEARATIONS:
         return {
           ...state,
-          dogsAll: action.payload,
-          requestDogs: action.payload,
+          addOperations: action.payload,
         };
-      case GET_DOGS_API:
-      case GET_DOGS_DB:
+      case GET_OPERATIONS:
         return {
           ...state,
-          requestDogs: action.payload,
+          getOperations: action.payload,
         };
-  
-      case GET_TEMPERAMENT:
-        const temp = action.payload.sort((a, b) => {
-          if (a.name > b.name) {
-            return 1;
-          } else {
-            return -1;
-          }
-        });
+      case UPDATE_OPERATIONS:
         return {
           ...state,
-          temperament: temp,
+          upateOperations: action.payload,
         };
-      case ADD_DOGS:
+      case DELETE_OPERATIONS:
         return {
           ...state,
-          addDogs: action.payload,
-        };
-      case SEARCH_BY_ID:
-        return {
-          ...state,
-          searchById: action.payload,
-        };
-      case DOG_DELETE:
-        return {
-          ...state,
-          requestDogs: state.dogsAll.filter((d) => d.id !== action.payload),
-        };
-  
-      case SEARCH_BY_NAME:
-        const imageUrl = state.dogsAll.filter((apiId) => {
-          let element;
-          for (let i = 0; i < action.payload.length; i++) {
-            if (apiId.image.id === action.payload[i]) {
-              return (element = apiId);
-            }
-          }
-          return element;
-        });
-  
-        return {
-          ...state,
-          // requestDogs: state.dogsAll.filter((b)=> {
-          //   return  Number(b.life_span[0] + b.life_span[1]) < action.payload
-          // }),
-           requestDogs: imageUrl.length > 0 ? imageUrl : action.payload,
+          deleteOperations: action.payload,
         };
   
-      case FILTER_BY_TEMPERAMENT:
-        if (action.payload === "All") {
-          return {
-            ...state,
-            requestDogs: state.dogsAll,
-          };
-        } else {
-          return {
-            ...state,
-            requestDogs: state.dogsAll.filter((po) => {
-              let prueba = po.temperaments.some((a) => {
-                return a.name === action.payload;
-              });
-              return prueba;
-            }),
-          };
-        }
-  
-      case GET_ORDER_DESC:
-      case GET_ORDER_ASC:
-        const ascDescFilter =
-          action.payload === "Ascendent" || action.payload === "All"
-            ? state.dogsAll.sort((a, b) => {
-                if (a.name > b.name) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-            : state.dogsAll.sort((a, b) => {
-                if (a.name < b.name) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              });
-        return {
-          ...state,
-          requestDogs: [...ascDescFilter],
-        };
-  
-      case GET_WEIGH_LIGHTER:
-      case GET_WEIGH_HIGHEST:
-        const filtered = state.dogsAll.filter((i) => {
-          return !i.weight.includes("NaN");
-        });
-        const orderWeigh =
-          action.payload === "Lighter"
-            ? filtered.sort((a, b) => {
-                if (
-                  Number(a.weight.split(" - ")[0]) >
-                  Number(b.weight.split(" - ")[0])
-                ) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-            : filtered.sort((a, b) => {
-                if (
-                  Number(a.weight.split(" - ")[0]) <
-                  Number(b.weight.split(" - ")[0])
-                ) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              });
-        return {
-          ...state,
-          requestDogs: [...orderWeigh],
-        };
-  
-      case GET_LIFE_MENOR:
-      case GET_LIFE_MAYOR:
-        const orderLifeSpan =
-          action.payload === "Young"
-            ? state.dogsAll.sort((a, b) => {
-                if (
-                  Number(a.life_span[0] + a.life_span[1]) >
-                  Number(b.life_span[0] + b.life_span[1])
-                ) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              })
-            : state.dogsAll.sort((a, b) => {
-                if (
-                  Number(a.life_span[0] + a.life_span[1]) <
-                  Number(b.life_span[0] + b.life_span[1])
-                ) {
-                  return 1;
-                } else {
-                  return -1;
-                }
-              });
-        return {
-          ...state,
-          requestDogs: [...orderLifeSpan],
-        };
-      default:
-        return state;
+
+
+
+
+
+
     }
-  }
-  export default RootReducer;
-  
+}
+export default RootReducer;
