b8f5b76943bb8cf41754c105e1bd8d6650184795
jose hernandez <josegho2013@gmail.com>
2022-01-07 12:03:37 -0300

    update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Form from "./Navbar";
+import DataTable from "react-data-table-component";
 import { useSelector, useDispatch } from "react-redux";
 import { getOperationsDb } from "../redux/actions/actions";
 import { Link } from "react-router-dom";
@@ -12,15 +13,63 @@ const Home = () => {
 
   const getOp = useSelector(({ getOperations }) => getOperations);
   console.log("getOp: ", getOp);
-  getOp.map((i)=>{
-    if(i.type === "Ingreso"){
-      return income = income + i.amount
+  getOp.map((i) => {
+    if (i.type === "Ingreso") {
+      return (income = income + i.amount);
     }
-    if(i.type === "Egreso"){
-      return expenses = expenses + i.amount
+    if (i.type === "Egreso") {
+      return (expenses = expenses + i.amount);
     }
-  })
+  });
 
+  const columns = [
+    {
+      name: "Income",
+      selector: (row) => {
+        if (row.type === "Ingreso") {
+          return row.amount;
+        }
+      },
+      sortable: true,
+      conditionalCellStyles: [
+        {
+          when: (row) => row.amount > 0,
+          style: {
+            color: "green",
+          },
+        },
+      ],
+      
+    },
+    {
+      name: "Expense",
+      selector: (row) => {
+        if (row.type === "Egreso") {
+          return row.amount;
+        }
+      },
+      sortable: true,
+      conditionalCellStyles: [
+        {
+          when: (row) => row.amount > 0,
+          style: {
+            color: "red",
+          },
+        },
+      ],
+    },
+    {
+      name: "Concept",
+      selector: (row) => row.concept,
+      sortable: true,
+    },
+
+    {
+      name: "Date",
+      selector: (row) => row.date,
+      sortable: true,
+    },
+  ];
 
   return (
     <div>
@@ -36,6 +85,8 @@ const Home = () => {
       <h2>{expenses}</h2>
 
       <h3>{income - expenses}</h3>
+
+      <DataTable columns={columns} data={getOp} title="Register Operations" />
     </div>
   );
 };
